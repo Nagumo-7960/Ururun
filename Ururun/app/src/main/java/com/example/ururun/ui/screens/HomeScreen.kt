03dc320b75d3502c8cd4cb0067c9577d963e5915
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-05-13 15:48:19 +0900

    背景画像を画面いっぱいに表示

--- Ururun/app/src/main/java/com/example/ururun/ui/screens/HomeScreen.kt
+++ Ururun/app/src/main/java/com/example/ururun/ui/screens/HomeScreen.kt
@@ -3,14 +3,16 @@ package com.example.ururun.ui.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.ururun.R
-import java.lang.reflect.Modifier
 
 @Composable
 fun HomeScreen(navController: NavController) {
@@ -23,6 +25,7 @@ fun HomeBackground() {
         painter = painterResource(id = R.drawable.home),
         contentDescription = "home_background",
         contentScale = ContentScale.FillBounds,
+        modifier = Modifier.fillMaxSize()
     )
 }
 
