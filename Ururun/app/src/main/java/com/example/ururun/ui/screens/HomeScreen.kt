b3730c82baefd9cdf89a0d533f72c5f56f4385df
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-05-13 15:35:13 +0900

    ホーム画面に背景画像を表示

--- Ururun/app/src/main/java/com/example/ururun/ui/screens/HomeScreen.kt
+++ Ururun/app/src/main/java/com/example/ururun/ui/screens/HomeScreen.kt
@@ -1,18 +1,34 @@
 package com.example.ururun.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.ururun.R
+import java.lang.reflect.Modifier
 
 @Composable
-fun HomeScreen(navController: NavController){
+fun HomeScreen(navController: NavController) {
+    HomeBackground()
+}
 
+@Composable
+fun HomeBackground() {
+    Image(
+        painter = painterResource(id = R.drawable.home),
+        contentDescription = "home_background",
+        contentScale = ContentScale.FillBounds,
+    )
 }
 
 @Preview
 @Composable
-fun PreviewHomeScreen(){
+fun PreviewHomeScreen() {
     val navController = rememberNavController()
     HomeScreen(navController)
 }
