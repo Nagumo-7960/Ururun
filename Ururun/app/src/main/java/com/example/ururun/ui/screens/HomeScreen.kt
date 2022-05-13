e3e668d80ac423634ef0fd120646eb89cdf17be8
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-05-13 16:28:47 +0900

    テキストを適切な位置に配置

--- Ururun/app/src/main/java/com/example/ururun/ui/screens/HomeScreen.kt
+++ Ururun/app/src/main/java/com/example/ururun/ui/screens/HomeScreen.kt
@@ -1,22 +1,43 @@
 package com.example.ururun.ui.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.ururun.R
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    HomeBackground()
+    Box (
+        modifier = Modifier.fillMaxSize()
+            ){
+        HomeBackground()
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            Column() {
+                Row (
+                    modifier = Modifier.fillMaxHeight(),
+                    verticalAlignment = Alignment.CenterVertically
+                        ){
+                    HomeTitleText()
+                }
+            }
+        }
+    }
 }
 
 @Composable
@@ -26,6 +47,13 @@ fun HomeBackground() {
         contentDescription = "home_background",
         contentScale = ContentScale.FillBounds,
         modifier = Modifier.fillMaxSize()
+    )
+}
+@Composable
+fun HomeTitleText(){
+    Text(
+        text = "うるるん",
+        fontSize = 50.sp,
     )
 }
 
