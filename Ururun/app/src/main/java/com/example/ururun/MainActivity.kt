fd7a278f6f6fd0b6cdca190ed8109773f00c4a36
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-05-14 21:38:50 +0900

    ボトムナビゲーションの配置に成功

--- Ururun/app/src/main/java/com/example/ururun/MainActivity.kt
+++ Ururun/app/src/main/java/com/example/ururun/MainActivity.kt
@@ -3,22 +3,57 @@ package com.example.ururun
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement.Bottom
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ururun.ui.theme.UrurunTheme
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Navigation()
+            val navController = rememberNavController()
+            Navigation(navController = navController)
+
+            Scaffold(
+                bottomBar = {
+                    BottomNavigationBar(
+                        items = listOf(
+                            BottomNavItem(
+                                name = "Buying",
+                                route = "buying",
+                                icon = Icons.Default.Home
+                            ),
+                            BottomNavItem(
+                                name = "Selling",
+                                route = "selling",
+                                icon = Icons.Default.Share
+                            )
+                        ),
+                        navController = navController,
+                        modifier = Modifier,
+                        onItemClick ={
+                            navController.navigate(it.route)
+                        }
+                    )
+                }
+            ) {
+                Navigation(navController = navController)
+            }
         }
     }
+
 }
 
 @Preview(showBackground = true)
--- Ururun/app/src/main/java/com/example/ururun/Navigation.kt
+++ Ururun/app/src/main/java/com/example/ururun/Navigation.kt
@@ -1,29 +1,76 @@
 package com.example.ururun
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.ururun.ui.screens.HomeScreen
 
+
 @Composable
-fun Navigation() {
-    val navController = rememberNavController()
+fun Navigation(navController: NavHostController) {
+
     NavHost(navController = navController, startDestination = "buying") {
         composable("buying") { BuyingScreen() }
         composable("selling") { SellingScreen() }
     }
 
+
 }
 
 @Composable
-fun ButtomNavigationBar(){
-    
+fun BottomNavigationBar(
+    items:List<BottomNavItem>,
+    navController:NavController,
+    modifier: Modifier,
+    onItemClick:(BottomNavItem) -> Unit
+){
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    BottomNavigation (
+        modifier = modifier,
+        backgroundColor = Color.DarkGray,
+        elevation = 5.dp
+            ){
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            BottomNavigationItem(
+                selected = selected,
+                onClick = { onItemClick(item) },
+                selectedContentColor = Color.Green,
+                unselectedContentColor = Color.Gray,
+                icon = {
+                    Column(horizontalAlignment = CenterHorizontally) {
+                        Icon(imageVector = item.icon, contentDescription = item.name)
+                    }
+                    if(selected){
+                        Text(text = item.name,
+                            textAlign = TextAlign.Center,
+                            fontSize = 10.sp
+                        )
+                    }
+                }
+
+                )
+        }
+    }
+
 }
 
 @Composable
