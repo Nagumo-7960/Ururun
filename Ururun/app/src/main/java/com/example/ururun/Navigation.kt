7a28bff7ef2d93f38e2d92cd60d0a5b8017cc39c
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-05-14 18:00:33 +0900

    購入画面と販売画面を作成

--- Ururun/app/src/main/java/com/example/ururun/Navigation.kt
+++ Ururun/app/src/main/java/com/example/ururun/Navigation.kt
@@ -1,6 +1,11 @@
 package com.example.ururun
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -9,8 +14,34 @@ import com.example.ururun.ui.screens.HomeScreen
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "homeScreen") {
-        composable("homeScreen") { HomeScreen(navController) }
+    NavHost(navController = navController, startDestination = "buying") {
+        composable("buying") { BuyingScreen() }
+        composable("selling") { SellingScreen() }
     }
 
+}
+
+@Composable
+fun ButtomNavigationBar(){
+    
+}
+
+@Composable
+fun BuyingScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "buy")
+    }
+}
+
+@Composable
+fun SellingScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "sell")
+    }
 }
