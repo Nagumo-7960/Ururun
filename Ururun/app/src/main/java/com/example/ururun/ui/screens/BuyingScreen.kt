a37d680c39591834bf13ac1f973dd4858187ebbf
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-05-15 22:38:30 +0900

    財布のアイコンを表示

--- Ururun/app/src/main/java/com/example/ururun/ui/screens/BuyingScreen.kt
+++ Ururun/app/src/main/java/com/example/ururun/ui/screens/BuyingScreen.kt
@@ -1,14 +1,14 @@
 package com.example.ururun.ui.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.ururun.R
@@ -17,15 +17,27 @@ import com.example.ururun.R
 fun BuyingScreen(navController: NavController) {
     Box(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
     ) {
-        Text(text = "buy")
+        Column() {
+            Row {
+                MyWalletIcon()
+            }
+        }
     }
 }
 
 @Composable
 fun MyWalletIcon(){
-    Image(painter = painterResource(id = R.drawable.yen), contentDescription = null)
+    Image(
+        painter = painterResource(id = R.drawable.yen),
+        contentDescription = "my_wallet_icon",
+        modifier = Modifier
+            .size(
+                width = 50.dp,
+                height = 50.dp
+            )
+    )
+
 }
 
 @Preview
