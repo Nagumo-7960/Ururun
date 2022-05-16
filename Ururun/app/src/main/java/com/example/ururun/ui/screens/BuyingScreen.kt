c8e160412d90843e8419c2c60f5467d3a852e80d
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-05-16 22:41:49 +0900

    二つのカードを表示

--- Ururun/app/src/main/java/com/example/ururun/ui/screens/BuyingScreen.kt
+++ Ururun/app/src/main/java/com/example/ururun/ui/screens/BuyingScreen.kt
@@ -1,14 +1,18 @@
 package com.example.ururun.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -21,23 +25,27 @@ fun BuyingScreen(navController: NavController) {
     Box(
         modifier = Modifier.fillMaxSize(),
     ) {
-        Column() {
-            Column(
-                modifier = Modifier
-                    .padding(
-                        top = 10.dp,
-                        start = 10.dp
-                    )
-            ) {
-                Row (verticalAlignment = Alignment.CenterVertically){
-                    MyWalletIcon()
-                    AmtMyWallet()
+        Column(modifier = Modifier.fillMaxSize()) {
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Column(
+                    modifier = Modifier
+                        .padding(
+                            top = 10.dp,
+                            start = 10.dp
+                        )
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        MyWalletIcon()
+                        AmtMyWallet()
+                    }
                 }
             }
 
-            Column() {
+
+            Column (){
                 Row {
-                    ProdForBuyCard()
+                    ProdForBuyCard_1()
+                    ProdForBuyCard_2()
                 }
             }
         }
@@ -47,7 +55,7 @@ fun BuyingScreen(navController: NavController) {
 }
 
 @Composable
-fun MyWalletIcon(){
+fun MyWalletIcon() {
     Image(
         painter = painterResource(id = R.drawable.yen),
         contentDescription = "my_wallet_icon",
@@ -61,7 +69,7 @@ fun MyWalletIcon(){
 }
 
 @Composable
-fun AmtMyWallet(){
+fun AmtMyWallet() {
     Text(
         text = "3,000,000",
         modifier = Modifier
@@ -73,9 +81,23 @@ fun AmtMyWallet(){
 }
 
 @Composable
-fun ProdForBuyCard(){
-    Card() {
-        Column {
+fun ProdForBuyCard_1() {
+    Card(
+        modifier = Modifier
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(50)
+            )
+            .padding(
+                top = 20.dp,
+                start = 20.dp,
+                end = 20.dp,
+                bottom = 20.dp
+
+            ),
+        elevation = 10.dp
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Image(
                 painter = painterResource(id = R.drawable.smartphone),
                 contentDescription = "smartphone",
@@ -86,16 +108,59 @@ fun ProdForBuyCard(){
                     ),
                 contentScale = ContentScale.Crop
             )
-            Text(text = "スマートフォン")
+            Text(
+                text = "スマートフォン",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
             Text(text = "30000円")
-        
+
+        }
+    }
+}
+
+@Composable
+fun ProdForBuyCard_2() {
+    Card(
+        modifier = Modifier
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(50)
+            )
+            .padding(
+                top = 20.dp,
+                start = 20.dp,
+                end = 20.dp,
+                bottom = 20.dp
+
+            ),
+        elevation = 10.dp
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = painterResource(id = R.drawable.fashion_watch),
+                contentDescription = "fashon_watch",
+                modifier = Modifier
+                    .size(
+                        width = 150.dp,
+                        height = 150.dp
+                    ),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = "スマートウォッチ",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(text = "20000円")
+
         }
     }
 }
 
 @Preview
 @Composable
-fun PreviewBuyingScreen(){
+fun PreviewBuyingScreen() {
     val navController = rememberNavController()
     BuyingScreen(navController)
 }
