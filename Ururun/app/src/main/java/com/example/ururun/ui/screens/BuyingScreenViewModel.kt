dd352a0c04911953cff06007fbcb2e9cc16f2c64
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-05-18 16:18:01 +0900

    LiveDataを追加

--- Ururun/app/src/main/java/com/example/ururun/ui/screens/BuyingScreenViewModel.kt
+++ Ururun/app/src/main/java/com/example/ururun/ui/screens/BuyingScreenViewModel.kt
@@ -1,4 +1,14 @@
 package com.example.ururun.ui.screens
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
 class BuyingScreenViewModel {
+    private val _amtMyWallet = MutableLiveData(3000000)
+    val amtMyWallet: LiveData<Int>get() = _amtMyWallet
+
+    fun RefeshMyWallet(productValue:Int){
+        _amtMyWallet.value = _amtMyWallet.value!! - productValue
+    }
+
 }
